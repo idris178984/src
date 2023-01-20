16656b2c59c7f08d1528aa3b501a97d487a40eaf
IdrisAkca <eylul178984@gmail.com>
2023-01-20 21:52:58 +0300

    day03 EscapeSequences

--- day03_EscapeSequences/Task.java
+++ day03_EscapeSequences/Task.java
@@ -4,11 +4,11 @@ public class Task {
     public static void main(String[] args) {
 
         int a=10;
-        System.out.println(a++);
-        System.out.println(a++);
-        System.out.println(--a);
-        System.out.println(--a);
-        System.out.println(a++);
+        System.out.println(a++);//10
+        System.out.println(a++);//11
+        System.out.println(--a);//11
+        System.out.println(--a);//10
+        System.out.println(a++);//10
 
     }
 }
