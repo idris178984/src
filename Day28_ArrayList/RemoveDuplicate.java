8c27f9734ae299efa5340466f628015097232c6f
IdrisAkca <eylul178984@gmail.com>
2023-02-03 11:40:19 +0300

    Day28 RemoveDuplicate

--- Day28_ArrayList/RemoveDuplicate.java
+++ Day28_ArrayList/RemoveDuplicate.java
@@ -31,6 +31,19 @@ public class RemoveDuplicate {
 
         System.out.println(result);
 
+        int []numbers1={1,2,3,4,5,6};
+        ArrayList<Integer> numbers2=new ArrayList<>();
+        for (int each : numbers1) {
+            numbers2.add(each);
+
+        }
+        numbers2.add(7);
+        numbers2.remove(2);
+
+        System.out.println(numbers2);
+
+
+
 
     }
 }
